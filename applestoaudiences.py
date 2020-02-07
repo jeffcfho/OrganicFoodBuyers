00475731249f6606f8c680dc3f9b8d4cf380c34e
jeffcfho <jeffreyh@gmail.com>
2020-02-06 20:52:49 -0500

    Update frontend to include actionable recommendations
    Also update for demo

--- applestoaudiences.py
+++ applestoaudiences.py
@@ -12,57 +12,71 @@ st.sidebar.subheader('A tool for targeting organic produce buyers')
 # import data (for this demo, the data lives locally, but in a more 
 #   secure application it would be retrieved from an external source)
 user_list = pd.read_csv('modeling_dfs/final_users_50k.csv')
-rec_list = pd.read_csv('top20_products_recom.csv')
+rec_list = pd.read_csv('top20_products_recom_purchasedbefore.csv')
+rec_list['new_item'] = rec_list['never_purchased_before'].map(lambda x: 'Yes' if x else 'No')
 prod_list = pd.read_csv('top200_products.csv')
 prod_list = prod_list.loc[prod_list['organic']==1]
 
+# Function for printing and formatting emails:
+def print_m_emails(emails_to_print,m):
+	if (len(emails_to_print)>=m):
+		st.code(',\n'.join(emails_to_print.sample(m).values)+'...')
+	else:
+		st.code(',\n'.join(emails_to_print.values)+'...')
+
 view_names = ['Target individual users','Get likely buyers','Get likely buyers by item','About']
 view = st.sidebar.radio('',view_names)
+num_emails_to_print = 20
 # Individual user view
 if view==view_names[0]:
-	# st.subheader()
 	#Sidebar elements
-	max_predicted_prob = st.sidebar.slider("Show users with probabilities less than",min_value=0.1,max_value=1.0,step=0.01,value=0.9)
-	option = st.sidebar.selectbox(
-	    'Select a user to see what products to recommend',
-	     user_list.loc[(user_list['predicted_prob']<max_predicted_prob),'user_dropdown'].head(20).values)
+	if st.sidebar.checkbox('Show only users who have not bought organic produce before'):
+		max_predicted_prob = st.sidebar.slider("Show users with probabilities less than",min_value=0.1,max_value=1.0,step=0.01,value=0.35)
+		option = st.sidebar.selectbox(
+		    'Select a user to see what products to recommend',
+		     user_list.loc[(user_list['predicted_prob']<max_predicted_prob),'user_dropdown'].head(20).values)
+	else: 
+		max_predicted_prob = st.sidebar.slider("Show users with probabilities less than",min_value=0.1,max_value=1.0,step=0.01,value=0.93)
+		option = st.sidebar.selectbox(
+		    'Select a user to see what products to recommend',
+		     user_list.loc[(user_list['predicted_prob']<max_predicted_prob),'user_dropdown'].head(20).values)
+	# if st.sidebar.checkbox('Show dataframe (internal)'):
+	# 	st.write(user_one)
 
 	selected_user_id = int(option[5:-9])
 	user_one = user_list.loc[user_list['user_id']==selected_user_id]
-	# if st.sidebar.checkbox('Show dataframe (internal)'):
-	# 	st.write(user_one)
 
 	#Main page elements
 	user_name = option[:-9] # Show about user
 	user_prob = user_one['predicted_prob'].values[0]
 	if (user_one['any_organic_produce_prev'].values[0]):
-		bought_produce = 'bought'  
+		bought_produce = 'bought'
+		recom = 'recommending <span style="color:red">new items</span> could grow sales'
 	else:
-		bought_produce = 'did not buy'  
+		bought_produce = 'not bought'  
+		recom = 'recommending <span style="color:red">popular organic items</span> could get them to buy organic'
 	pct_organic = 100*user_one['pct_organic'].values[0]
 	st.markdown(f"""
 		## {view_names[0]}
 		### <span style="color:green">{user_name}</span> will next buy organic produce with a probability of <span style="color:red">{user_prob:.2f}</span>.
-		What would increase the probability?
-		- This user <span style="color:green">{bought_produce}</span> organic produce in their most previous order.
-		- <span style="color:green">{pct_organic:.0f}%</span> of their top 20 recommendations are organic, compared to the 52% user average.
+		- They have <span style="color:green">{bought_produce}</span> organic produce before, so {recom}.
 		""",unsafe_allow_html=True)
 
 	# Show top recommendations for user
-	rec_user = rec_list.loc[(rec_list['user_id']==selected_user_id)&(rec_list['organic']==1),['product_name','freq_rank']]
+	rec_user = rec_list.loc[(rec_list['user_id']==selected_user_id)&(rec_list['organic']==1),['product_name','freq_rank','new_item']]
 	rec_user.set_index(np.arange(1,len(rec_user)+1,1),inplace=True)
-	rec_user.columns = ['Recommended items','Item popularity rank']
+	rec_user.columns = ['Recommended items','Item popularity rank','New item?']
 	st.dataframe(rec_user)
 # List of users view
 elif view==view_names[1]:
 	st.markdown(f"## {view_names[1]}")
-	min_predicted_prob = st.slider("Show users with probabilities greater than",min_value=0.5,max_value=0.99,step=0.01,value=0.9)
+	min_predicted_prob = st.slider("Show users with probabilities greater than",min_value=0.5,max_value=0.99,step=0.01,value=0.88)
 	user_emails = user_list.loc[(user_list['predicted_prob']>min_predicted_prob),'user_emails']
 
 	st.markdown(f"""
-		There are <span style="color:red">{len(user_emails)}</span> users to target. A random sample of 20 is shown below:
+		There are <span style="color:red">{len(user_emails)}</span> users to target. A random sample of at most {num_emails_to_print} is shown below:
 		""",unsafe_allow_html=True)
-	st.code(',\n'.join(user_emails.sample(20).values)+'...')
+	print_m_emails(user_emails,num_emails_to_print)
 elif view==view_names[2]:
 	st.markdown(f"## {view_names[2]}")
 	# Get list of users who were recommended a product
@@ -77,19 +91,16 @@ elif view==view_names[2]:
 	st.markdown(f"""
 		There are <span style="color:red">{len(user_emails)}</span> users to target who are likely to buy the item <span style="color:green">[{prod_option}]</span>.
 		
-		A random sample of at most 20 is shown below:
+		A random sample of at most {num_emails_to_print} is shown below:
 		""",unsafe_allow_html=True)
-	if (len(user_emails)>=20):
-		st.code(',\n'.join(user_emails.sample(20).values)+'...')
-	else:
-		st.code(',\n'.join(user_emails.values)+'...')
+	print_m_emails(user_emails,num_emails_to_print)
 # About page
 elif view==view_names[3]:
 	st.markdown(f"""
 		## {view_names[3]}
 		Organic food is the <span style="font-weight:bold">fastest growing category in retail grocery</span> today, but still represents <span style="color:green;font-weight:bold">just 6% of the total market share</span> in the U.S. To retain and continue to grow the market, <a href="https://maccabee.com/case_study/building-awareness-sales-for-organic-food-products/" target="_blank">organic trade associations provide coupons and other advertising</a> to customers to incentivize purchases.
 
-		Apples to Audiences is a web app built upon an API that <span style="font-weight:bold">identifies customers who are likely to buy organic produce next and recommends items for them</span>, based on their past shopping history. The model blend results from collaborative filtering and logistic regression models to provide both a likelihood of purchase plus specific items to recommend. 
+		Apples to Audiences is a web app built upon an API that <span style="font-weight:bold">identifies customers who are likely to buy organic produce next and recommends organic products for them</span>, based on their past shopping history. The models blend results from collaborative filtering and logistic regression models to provide both a likelihood of purchase plus specific items to recommend. 
 		""",unsafe_allow_html=True)
 	#Show histogram of probabilities
 	f = px.histogram(user_list, x="predicted_prob", nbins=20, title="User distribution",
@@ -99,7 +110,7 @@ elif view==view_names[3]:
 	st.plotly_chart(f)
 
 	st.markdown("""
-		Compared to traditional marketing and demographics surveys, this allows for more focused targeting based on actual past purchases. More focused targeting will <span style="color:green;font-weight:bold">increase lift in the percentage of purchases with organic items</span>.
+		Compared to non-targeted campaigns, this model identifies<span style="color:green;font-weight:bold"> 6% new buyers</span> of organic produce while <span style="color:green;font-weight:bold">reducing non-relevant spam by >44%</span> for unlikely buyers.
 
 		The data used to train the model come from 3.4 million orders made by 200k users made public by <a href ="https://www.instacart.com/datasets/grocery-shopping-2017" target="_blank">Instacart</a>.
 		""",unsafe_allow_html=True)
